fa794a3c98b07aadf92dd826e00f7010554fdaad
Javier Reyes <jhreyes483@misena.edu.co>
2021-02-18 22:20:44 -0500

    Shore

--- _models/class.conexion.php
+++ _models/class.conexion.php
@@ -3,22 +3,6 @@
 
 
 /*
-        static function conexionPDO(){
-            $DB_HOST = 'localhost';
-            $DB_USER = 'root';
-            $DB_PASS = '';
-            $DB_NAME = 'sicloud';
-    
-            try {
-                $dsn = "mysql:host=$DB_HOST;dbname=$DB_NAME";
-                $db = new PDO($dsn, $DB_USER,  $DB_PASS);
-            } catch (PDOException $e) {
-                echo 'Error al conectarnos al; ' . $e->getMessage();
-            }
-            return $db;
-        }
-
-
     static function conexionPDO(){
         $DB_HOST = 'localhost';
         $DB_USER = 'root';
